4de34e97f2b339c2203b1ab353b06da0233f1cdb
Salik47 <msalik32@gmail.com>
2023-01-16 19:06:56 +0530

    Scheduling added

--- controllers/sendMessage.js
+++ controllers/sendMessage.js
@@ -12,14 +12,18 @@ const sendMessage = async () => {
 
   const index = hour - 9;
 
-  if(minutes === 30) {
+  if (minutes === 30) {
     // send quote
+    console.log(getQuote());
   } else {
-    if(index - 1 >= 0) {
-      // get schedule at index
-      // remember at a time there would be something ending and next thing starting
-      // check if last task is completed and next task is starting
-      // mesage: It's time to start ${schedule[index]} task was ${schedule[index-1]} completed?
+    if (index - 1 >= 0) {
+      console.log(
+        `It's time to start ${parsedSchedule[index]} task was ${
+          parsedSchedule[index - 1]
+        } completed?`
+      );
+    } else if (index === 0) {
+      console.log(`It's time to start ${parsedSchedule[index]}`);
     }
   }
 };
